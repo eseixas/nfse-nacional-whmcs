555b8f983c2b9d96e74405014bb92601ecc89873
Seixas Tecnologia <contato@seixastecnologia.com.br>
2026-05-06 19:30:50 -0300

    Cleanup test scripts

--- test_server_time.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "Current Time: " . date('Y-m-d\TH:i:sP') . "\n";
-echo "Current Time - 600: " . date('Y-m-d\TH:i:sP', time() - 600) . "\n";
-echo "Current Time - 3600 (1h): " . date('Y-m-d\TH:i:sP', time() - 3600) . "\n";
-echo "Current Time - 10800 (3h): " . date('Y-m-d\TH:i:sP', time() - 10800) . "\n";
--- test_time.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "Current Time: " . date('Y-m-d\TH:i:sP') . "\n";
-echo "Current Time - 600: " . date('Y-m-d\TH:i:sP', time() - 600) . "\n";
